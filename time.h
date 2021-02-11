f83416cccd100874a6ca51781f1949256822972e
Aaron C Gaudette <acg@aaronsee.media>
2021-02-11 08:56:58 +0000

    Make fixed accumulator conditional

--- time.h
+++ time.h
@@ -2,10 +2,6 @@
 #define MIN_DT 1e-4
 #endif
 
-#ifndef FIXED_DT
-#define FIXED_DT (1.f / 30.f)
-#endif
-
 extern struct _time {
 	struct {
 		float real;
@@ -17,11 +13,12 @@ extern struct _time {
 		float game;
 	} dt;
 
+#ifdef FIXED_DT
 	struct {
 		float acc;
 		float rem;
 	} fixed;
-
+#endif
 	float scale; // Remember to initialize!
 } _time;
 
@@ -37,10 +34,13 @@ static int _time_tick(float *t, float *dt)
 	_time.el.game += *dt;
 	*t = _time.el.game;
 
+#ifdef FIXED_DT
 	_time.fixed.acc += _time.dt.game;
+#endif
 	return *dt > 0.f;
 }
 
+#ifdef FIXED_DT
 static int _time_step()
 {
 	if (_time.fixed.acc < FIXED_DT) {
@@ -52,3 +52,4 @@ static int _time_step()
 	_time.fixed.acc -= FIXED_DT;
 	return 1;
 }
+#endif
