a25013181fee8b9e2986ca211e65035b9e024291
Aaron C Gaudette <acg@aaronsee.media>
2022-01-14 08:02:24 -0500

    Add panic_perr

--- sys.h
+++ sys.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 __attribute__((noreturn))
 static void panic()
@@ -16,7 +18,14 @@ __attribute__((noreturn))
 static void panic_msg(const char *msg)
 {
 	fflush(stdout);
-	fprintf(stderr, "panic(): %s\n", msg);
+	fprintf(stderr, "panic! %s\n", msg);
+	panic();
+}
+
+__attribute__((noreturn))
+static void panic_perr(const char *msg)
+{
+	fprintf(stderr, "panic! %s: %s\n", msg, strerror(errno));
 	panic();
 }
 
