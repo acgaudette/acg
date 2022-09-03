67916391f408191cc637e361767b2ecce78065bf
Aaron C Gaudette <acg@aaronsee.media>
2022-09-02 22:47:25 -0400

    Implement compile time conditional macros

--- rule10.h
+++ rule10.h
@@ -1,3 +1,36 @@
+#define ACG_EMPTY()
+#define ACG_DEFER(ID) ID ACG_EMPTY() // (ARGS)
+#define ACG_CONSUME(...)
+#define ACG_EVAL(...) __VA_ARGS__
+
+#define JOIN_2(A, B) A ## B
+#define JOIN_3(A, B, C) A ## B ## C
+#define JOIN(A, B) JOIN_2(A, B)
+
+#define ACG_IF___ELSE(...) /* CONSUME */ ACG_EVAL
+#define ACG_IF_0_ELSE(...) /* CONSUME */ ACG_EVAL
+#define ACG_IF_1_ELSE(...)  __VA_ARGS__  ACG_CONSUME
+#define IF_01_ELSE(COND) JOIN_3(ACG_IF_, COND, _ELSE) // (THEN) (ELSE)
+
+#define ACG_IF__(...) /* CONSUME */
+#define ACG_IF_0(...) /* CONSUME */
+#define ACG_IF_1(...)  __VA_ARGS__
+#define IF_01(COND) JOIN(ACG_IF_, COND) // (THEN)
+
+#define FST(FST, ...)      FST
+#define SND(___, SND, ...) SND
+
+#define ACG_IS_ONE(...) SND(__VA_ARGS__, 0)
+#define ACG_ONE $, 1
+
+#define ACG_NOT__ ACG_ONE
+#define ACG_NOT_0 ACG_ONE
+#define NOT(_) ACG_IS_ONE(JOIN(ACG_NOT_, _))
+#define CAST_BOOL(_) NOT(NOT(_))
+
+#define IF_ELSE(COND) IF_01_ELSE(CAST_BOOL(COND)) // (THEN) (ELSE)
+#define IF(COND) IF_01(CAST_BOOL(COND)) // (THEN)
+
 #define PASTE_1(...) __VA_ARGS__
 #define PASTE_2(...)  PASTE_1(__VA_ARGS__)  PASTE_1(__VA_ARGS__)
 #define PASTE_3(...)  PASTE_2(__VA_ARGS__)  PASTE_1(__VA_ARGS__)
@@ -30,7 +63,3 @@
 #define PASTE_30(...) PASTE_16(__VA_ARGS__) PASTE_14(__VA_ARGS__)
 #define PASTE_31(...) PASTE_16(__VA_ARGS__) PASTE_15(__VA_ARGS__)
 #define PASTE_32(...) PASTE_16(__VA_ARGS__) PASTE_16(__VA_ARGS__)
-
-#define ACG_EMPTY()
-#define ACG_DEFER(ID) ID ACG_EMPTY() // (ARGS)
-#define ACG_EVAL(...) __VA_ARGS__
